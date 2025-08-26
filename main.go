f518d5f4fb940a283c94dec3fb780a9097622ca2
rizzzabh <rishabhmishra963010@gmail.com>
2025-08-27 00:31:56 +0530

    classes

--- main.go
+++ main.go
@@ -2,11 +2,25 @@ package main
 
 import "fmt"
 
+type car struct {
+	make string 
+	model string 
+}
+type truck struct {
+	car 
+	bigWheel int 
+}
 func main(){
-	const name = "Rishabh" 
-	const surName = "Mishra" 
+	// const name = "Rishabh" 
+	// const surName = "Mishra" 
 
-	msg := fmt.Sprintf("Hi %v %v, welcome to my Club" , name , surName) 
+	// msg := fmt.Sprintf("Hi %v %v, welcome to my Club" , name , surName) 
 
-	fmt.Println(msg)
+	// fmt.Println(msg)
+
+	truckDriver := truck{} 
+
+	truckDriver.model = "AmitabhBachhan" 
+
+	fmt.Println(truckDriver.model)
 }
