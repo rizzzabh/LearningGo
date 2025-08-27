7a6bf32b1df1ba383858bfa887eaa037becf9bc6
rizzzabh <rishabhmishra963010@gmail.com>
2025-08-28 00:40:33 +0530

    Loop

--- main.go
+++ main.go
@@ -1,42 +1,22 @@
 package main 
  
-import ("errors"  
-	"fmt")
+import ("fmt")
 
-type error interface {
-	Error() string 
-}
-
-func divide (x int , y int) (int , error ){
-	if (y == 0){
-		return 0 , errors.New("Divide by zero error")
-	}
-
-	return x / y , nil 
-}
-
-type MyError struct {
-	Code int 
-	Msg string 
-}
-
-func (e MyError) Error() string {
-	return fmt.Sprintf("Error code : %f and message : %s" , e.Code , e.Msg)
-}
-
-func risky() error {
-	return MyError{Code: 404 , Msg: "Not Found"}
-}
 func main(){
-	result , err := divide(10 , 2)
-	risk := risky() 
 
-	if risk != nil {
-		fmt.Println("Oops , " , risk)
+	var num int 
+	fmt.Scan(&num)
+	for i := 1 ; i <= num ; i++ {
+		isPrime := true
+		for j := 2 ; j*j <= i ; j++ {
+			if i % j == 0 {
+				isPrime = false 
+				break 
+			}
+		}
+		if isPrime {
+			fmt.Print(i , " ") 
+		}
 	}
-	if err != nil {
-		fmt.Println("Error: " , err) 
-	}else {
-		fmt.Println("Result: " , result)
-	}
+	fmt.Println()
 }
