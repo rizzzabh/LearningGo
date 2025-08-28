f77b422f3968a87de7a47fc1de89e010bbfcc1c0
rizzzabh <rishabhmishra963010@gmail.com>
2025-08-28 23:50:28 +0530

    wrapping up slices

--- main.go
+++ main.go
@@ -5,9 +5,18 @@ import ("fmt")
 
 func main(){
 	slice := make([]int , 2 , 3)
-	
+
 	slice = append(slice , 4 , 5) 
 
 	fmt.Print(slice)
+	arr := [6]int{1 ,2 ,3 , 4 , 5 , 6}
 
+	slice2 := arr[1 : 4]
+
+	fmt.Println(slice2)
+
+	for i , v := range arr {
+		fmt.Println(i , v)
+	}
+	
 }
