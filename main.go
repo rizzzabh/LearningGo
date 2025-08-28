ba7002df28845d54b252d78a1a7595026bb6baf0
rizzzabh <rishabhmishra963010@gmail.com>
2025-08-28 23:34:28 +0530

    learnig slices

--- main.go
+++ main.go
@@ -3,20 +3,6 @@ package main
 import ("fmt")
 
 func main(){
-
-	var num int 
-	fmt.Scan(&num)
-	for i := 1 ; i <= num ; i++ {
-		isPrime := true
-		for j := 2 ; j*j <= i ; j++ {
-			if i % j == 0 {
-				isPrime = false 
-				break 
-			}
-		}
-		if isPrime {
-			fmt.Print(i , " ") 
-		}
-	}
-	fmt.Println()
+	var s []int 
+	fmt.Print(s , len(s) , cap(s))
 }
