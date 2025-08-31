b68ee4c06771323bbc37d65dc235bc128607af78
rizzzabh <rishabhmishra963010@gmail.com>
2025-09-01 01:08:38 +0530

    Pointer

--- main.go
+++ main.go
@@ -2,22 +2,12 @@ package main
  
 import ("fmt")
 
-func variadic (greeting string , values ...interface {}){
-	for _ , v := range values {
-		fmt.Println(greeting , v) 
-	}
-}
-func sum (nums ...int) int {
-	s := 0 
-	for _ , value := range nums {
-		s += value 
-	}
-	return s 
-}
 func main(){
-	slices := []int {1 , 2 , 3 , 4}
-	totSum := sum (slices...)
+	var x int = 50 
 
-	fmt.Println(totSum)
-	variadic("Hello" , 10 , 3.15 , "Rishabh")
+	var y *int = &x 
+
+	*y = 100 
+
+	fmt.Print(x)
 }
